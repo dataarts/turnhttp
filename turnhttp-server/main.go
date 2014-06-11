b6020a2fa42449d17116f9a9759c09584b869cd8
Doug Fritz <dougfritz@gmail.com>
2014-06-11 16:57:42 -0500

    inital version with url fetching

---
+++ turnhttp-server/main.go
@@ -0,0 +1,100 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/doug/turnhttp"
+)
+
+var (
+	port       = flag.String("port", "8080", "port to run on")
+	servers    = flag.String("servers", "", "comma seperated list of turn server IPs")
+	serversUrl = flag.String("servers-url", "", "json resource returning list of turn server uris")
+	domains    = flag.String("domains", "", "comma seperated list of acceptable domains")
+	domainsUrl = flag.String("domains-url", "", "json resource returning list of acceptable domains")
+	secret     = flag.String("secret", "notasecret", "shared secret to use")
+	secretUrl  = flag.String("secret-url", "", "json resource returning shared secret to use")
+	rateString = flag.String("rate", "30s", "rate of url updating e.g. 30s or 1m15s")
+	rate       time.Duration
+	domainList []string
+	uris       []string
+	mutex      = new(sync.Mutex)
+)
+
+func update(url string, ptr interface{}) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	decoder := json.NewDecoder(resp.Body)
+	mutex.Lock()
+	err = decoder.Decode(ptr)
+	mutex.Unlock()
+	return err
+}
+
+func synchronize(url string, ptr interface{}) {
+	for {
+		update(url, ptr)
+		time.Sleep(rate)
+	}
+}
+
+// run a server
+func main() {
+	flag.Parse()
+	var err error
+	rate, err = time.ParseDuration(*rateString)
+	if err != nil {
+		panic(err)
+	}
+	for _, ip := range strings.Split(*servers, ",") {
+		uris = append(uris, fmt.Sprintf("turn:%s:3478?transport=udp", ip))
+		uris = append(uris, fmt.Sprintf("turn:%s:3478?transport=tcp", ip))
+		uris = append(uris, fmt.Sprintf("turn:%s:3479?transport=udp", ip))
+		uris = append(uris, fmt.Sprintf("turn:%s:3479?transport=tcp", ip))
+	}
+	domainList = strings.Split(*domains, ",")
+
+	turn := &turnhttp.Service{
+		Secret:  *secret,
+		Uris:    uris,
+		Domains: domainList,
+	}
+
+	if *serversUrl != "" {
+		go synchronize(*serversUrl, &turn.Uris)
+	}
+	if *domainsUrl != "" {
+		go synchronize(*domainsUrl, &turn.Domains)
+	}
+	if *secretUrl != "" {
+		go synchronize(*secretUrl, &turn.Secret)
+	}
+
+	http.Handle("/", turn)
+
+	fmt.Printf("Starting turnhttp on port %v\n", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
--- turnhttp.go
+++ turnhttp.go
@@ -12,34 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package turnhttp
 
 import (
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"strings"
 	"time"
 )
 
-var (
-	port       = flag.String("port", "8080", "port to run on")
-	servers    = flag.String("servers", "", "comma seperated list of turn server IPs")
-	domains    = flag.String("domains", "", "comma seperated list of acceptable domains")
-	secret     = flag.String("secret", "notasecret", "shared secret to use")
-	domainList []string
-	uris       []string
-)
-
-func credentials(username string) (user, pass string) {
+func credentials(username, secret string) (user, pass string) {
 	timestamp := time.Now().Unix()
 	user = fmt.Sprintf("%d:%s", timestamp, username)
-	h := hmac.New(sha1.New, []byte(*secret))
+	h := hmac.New(sha1.New, []byte(secret))
 	h.Write([]byte(user))
 	pass = base64.StdEncoding.EncodeToString(h.Sum(nil))
 	return
@@ -51,7 +39,13 @@ type TurnResponse struct {
 	Uris     []string `json:"uris"`
 }
 
-func Handle(rw http.ResponseWriter, r *http.Request) {
+type Service struct {
+	Domains []string
+	Secret  string
+	Uris    []string
+}
+
+func (self *Service) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	if username == "" {
 		http.Error(rw, "Must supply a username.", http.StatusBadRequest)
@@ -59,8 +53,7 @@ func Handle(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	origin := r.URL.Host
-	fmt.Println(origin, domainList)
-	for _, domain := range domainList {
+	for _, domain := range self.Domains {
 		if origin == domain {
 			goto ACCEPT
 		}
@@ -69,31 +62,14 @@ func Handle(rw http.ResponseWriter, r *http.Request) {
 	return
 
 ACCEPT:
-	user, pass := credentials(username)
+	user, pass := credentials(username, self.Secret)
 	resp := TurnResponse{
 		Username: user,
 		Password: pass,
-		Uris:     uris,
+		Uris:     self.Uris,
 	}
 
 	rw.Header().Set("Content-Type", "application/json")
 	enc := json.NewEncoder(rw)
 	enc.Encode(resp)
-}
-
-// run a server
-func main() {
-	flag.Parse()
-	for _, ip := range strings.Split(*servers, ",") {
-		uris = append(uris, fmt.Sprintf("turn:%s:3478?transport=udp", ip))
-		uris = append(uris, fmt.Sprintf("turn:%s:3478?transport=tcp", ip))
-		uris = append(uris, fmt.Sprintf("turn:%s:3479?transport=udp", ip))
-		uris = append(uris, fmt.Sprintf("turn:%s:3479?transport=tcp", ip))
-	}
-	domainList = strings.Split(*domains, ",")
-
-	http.HandleFunc("/", Handle)
-
-	fmt.Printf("Starting turnhttp on port %v\n", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
